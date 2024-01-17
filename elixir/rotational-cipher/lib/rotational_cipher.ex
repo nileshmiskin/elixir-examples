528d080a6fe29ef86d14a3c34f78173d8afe0249
nileshmiskin <nileshmiskin@gmail.com>
2024-01-17 16:39:55 +0530

    Solve rotational-cipher - Charlist - Range

---
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -0,0 +1,28 @@
+defmodule RotationalCipher do
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) do
+    text
+    |> String.to_charlist()
+    |> Enum.map(&rotate_char(&1, shift))
+    |> List.to_string()
+  end
+
+  defp rotate_char(char, 0), do: char
+
+  defp rotate_char(char, shift) when char in ?a..?z do
+    rem(char - ?a + shift, 26) + ?a
+  end
+
+  defp rotate_char(char, shift) when char in ?A..?Z do
+    rem(char - ?A + shift, 26) + ?A
+  end
+
+  defp rotate_char(char, _shift), do: char
+end
