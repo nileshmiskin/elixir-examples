feace9add52365e4b5eb02f3b8ab0716f04e87b0
nileshmiskin <nileshmiskin@gmail.com>
2024-01-17 12:33:47 +0530

    Solve resistor-color-duo - Atom - Map - List

---
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -0,0 +1,19 @@
+defmodule ResistorColorDuo do
+  @doc """
+  Calculate a resistance value from two colors
+  """
+  @color_values %{
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+  @spec value(colors :: [atom]) :: integer
+  def value([color1, color2 | _rest]), do: @color_values[color1] * 10 + @color_values[color2]
+end
